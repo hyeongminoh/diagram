be57ee9bcd1ec410c106da8b42a51b266991a87b
hyeongminoh <mihn0621@gmail.com>
2018-06-13 04:07:23 +0900

    회원가입까지

--- routes/router.js
+++ routes/router.js
@@ -32,4 +32,37 @@ module.exports = function(app){
     res.render('post');
   });
 
+  app.get('/signup', function(req, res, next) {
+    res.render('signup');
+  });
+
+  app.get('/signin', function(req, res, next) {
+    res.render('signin');
+  });
+
+
+  //회원 가입 진행 코드
+   app.post("/user_registration", function (req,res){
+          console.log("user_registration connect");
+          var body = req.body;
+          var email = body.email;
+          var name = body.name;
+          var passwd = sha256(body.password);
+          var nickname = body.nickname;
+         console.log(email, name);
+
+         db.query('INSERT INTO user(user_email, user_pw, user_name, user_nickname, user_image) VALUES(?,?,?,?,?) ',
+         [email, passwd, name, nickname, 'image'], function(error,result){
+            if(error) throw error;
+            console.log('추가 완료. result: ',email, passwd, name, nickname);
+            // alert("회원 가입이 완료되었습니다.");
+            res.redirect(url.format({
+                     pathname: '/signin',
+                     query: {
+                           'success': true,
+                           'message': 'Sign up success'
+                     }
+            }));
+         });
+       });
 }
